14b1c5e4b96a65dd5bfd839341dccd5e92c64d13
unknown <fazeelimam6@gmail.com>
2026-03-05 02:00:02 +0500

    chnages

--- backEnd/Middleware/auth.js
+++ backEnd/Middleware/auth.js
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 
-const auth = async(req,res, next)=>{
+const auth = async (req, res, next) => {
     const token = req.headers.authorization;
 
     try {
         jwt.verify(token, process.env.JWT_SECRET);
         next();
     } catch (error) {
-        res.json({success: false, message:"Invalid token"});
+        res.json({ success: false, message: "Invalid token" });
     }
 }
 
